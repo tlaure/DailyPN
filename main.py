4f83e72c3518d5a106ba7a5018221ae3d44bad7b
Thomas Laure <thomas.laure@me.com>
2019-01-24 14:03:45 +0800

    V1

--- main.py
+++ main.py
@@ -7,111 +7,258 @@ Created on Sat Jan 12 09:29:59 2019
 """
 
 """Main process"""
-
-"""restart the databases"""
-
-import sqlite3
-conn = sqlite3.connect('Data/investData.db')
-cur = conn.cursor()
-
-cur.execute("drop table if exists orders")
-conn.commit()
-
-cur.execute("create table orders (date string, type text, ticker string, quantity real, pru real)")
-conn.commit()
-
-conn = sqlite3.connect('Data/investData.db')
-cur.execute("drop table if exists priceTab")
-conn.commit()
-
-cur.execute("create table priceTab ( date string, ticker text, price real)")
-conn.commit()
-
-conn.close()
-"""end the databases"""
-
-"""add orders"""
-date=input('Date ? (format yyyy-mm-dd)')
-ttype=input('B/S ?')
-ticker=input('Ticker ?')
-quantity=input('Quantity ?')
-pru=input('PRU ?')
-
-conn = sqlite3.connect('Data/investData.db')
-cur = conn.cursor()
-cur.execute("insert into orders values (?, ?, ?, ?, ?)",(date , ttype , ticker , quantity , pru ))
-conn.commit()
-conn.close
-"""end add orders"""
-
-
-"""import past prices """
-import pandas as pd
-pastPrice = pd.read_csv('Data/initialPrices.csv')
-
-conn = sqlite3.connect('Data/investData.db')
-cur = conn.cursor()
-
-for iLine in range(len(pastPrice)):
-    cur.execute("insert into priceTab values (?, ?, ?)",(pastPrice['date'][iLine] , pastPrice['ticker'][iLine] , pastPrice['close'][iLine] ))
-    conn.commit()
-conn.close
-"""end add orders"""
-
-
-"""loop job"""
-#import the sql data 
-
 import sqlite3
 import numpy as np
-import datetime
-conn = sqlite3.connect('Data/investData.db')
-cur = conn.cursor()
-#Place the cursor on the begining of the db
-cur.execute('Select * FROM orders')
-orders=cur.fetchall()
-cur.execute('Select * FROM priceTab')
-priceHist=cur.fetchall()
-conn.close
+import pandas as pd
 
-#convert to a dataframe
-orders=pd.DataFrame(orders,columns=['date' , 'ttype' , 'ticker' , 'quantity' , 'pru' ])
-priceHist=pd.DataFrame(priceHist,columns=['date' , 'ticker' , 'price' ])
+def restart_all():
+    """restart the databases"""
+    conn = sqlite3.connect('Data/investData.db')
+    cur = conn.cursor()
+    
+    cur.execute("drop table if exists orders")
+    conn.commit()
+    
+    cur.execute("create table orders (date string, type text, ticker string, quantity real, pru real)")
+    conn.commit()
+    
+    cur.execute("drop table if exists priceTab")
+    conn.commit()
+    
+    cur.execute("create table priceTab ( date string, ticker text, price real)")
+    conn.commit()
+    
+    conn.close()
+    """end the databases"""
 
-#Define the time vector
+def add_order():
+    """add orders"""
+    date=input('Date ? (format yyyy-mm-dd)')
+    ttype=input('B/S ?')
+    ticker=input('Ticker ?')
+    quantity=input('Quantity ?')
+    pru=input('PRU ?')
+    
+    conn = sqlite3.connect('Data/investData.db')
+    cur = conn.cursor()
+    cur.execute("insert into orders values (?, ?, ?, ?, ?)",(date , ttype , ticker , quantity , pru ))
+    conn.commit()
+    conn.close
+    """end add orders"""
 
-newIndex=orders.sort_values('date').index #sort by date
-allTickers=orders['ticker'].unique()
-today=datetime.datetime.now().strftime("%Y-%m-%d")
-yesterday=datetime.datetime.now()-datetime.timedelta(days=1)
-yesterday=yesterday.strftime("%Y-%m-%d")
-invDate=priceHist['date'].unique()
+def import_past_prices():
+    """import past prices """
+    pastPrice = pd.read_csv('Data/initialPrices.csv')
+    
+    conn = sqlite3.connect('Data/investData.db')
+    cur = conn.cursor()
+    
+    for iLine in range(len(pastPrice)):
+        cur.execute("insert into priceTab values (?, ?, ?)",(pastPrice['date'][iLine] , pastPrice['ticker'][iLine] , pastPrice['close'][iLine] ))
+        conn.commit()
+    conn.close
+    """end import"""
 
-fundComp=np.zeros((len(invDate),len(allTickers)))
-fundVal=np.zeros((len(invDate),len(allTickers)))
-fundVal=np.zeros((len(invDate),len(allTickers)))
-fundPMV=np.zeros((len(invDate),len(allTickers)))
+def loop_job():
+    """loop job"""
+    #import the sql data 
+    
+    import datetime
+    import requests
+   
+    from bs4 import BeautifulSoup
+    
+    day=datetime.datetime.now().isoweekday()
+    if day != 1 or day != 7: #No news on sundays & monday
+            
+        
+        conn = sqlite3.connect('Data/investData.db')
+        cur = conn.cursor()
+        #Place the cursor on the begining of the db
+        cur.execute('Select * FROM orders')
+        orders=cur.fetchall()
+        cur.execute('Select * FROM priceTab')
+        priceHist=cur.fetchall()
+        conn.close
+        
+        #convert to a dataframe
+        orders=pd.DataFrame(orders,columns=['date' , 'ttype' , 'ticker' , 'quantity' , 'pru' ])
+        priceHist=pd.DataFrame(priceHist,columns=['date' , 'ticker' , 'price' ])
+        
+        #Define the time vector
+        
+        newIndex=orders.sort_values('date').index #sort by date
+        allTickers=orders['ticker'].unique()
+        today=datetime.datetime.now().strftime("%Y-%m-%d")
+        yesterday=datetime.datetime.now()-datetime.timedelta(days=1)
+        yesterday=yesterday.strftime("%Y-%m-%d")
+        
+        
+        #Add yesterday to the base
+        for tick in allTickers:
+            site = "https://finance.yahoo.com/quote/"+tick+"?p="+tick
+            page = requests.get(site).text #Import all html data as text
+            soup = BeautifulSoup(page,'lxml')
+            table = soup.find('td', {'data-test':'PREV_CLOSE-value'})
+            lastClose=table.string.strip()
+            priceHist.loc[len(priceHist)] = [yesterday,tick,float(lastClose)]
+            #Add to the sql base !! commented for test purposes
+            #conn = sqlite3.connect('Data/investData.db')
+            #cur = conn.cursor()
+            #cur.execute("insert into priceTab values (?, ?, ?)",(yesterday , tick , lastClose))
+            #conn.commit()
+            #conn.close
+        
+        invDate=priceHist['date'].unique()
+        invDate.sort()
+        
+        fundComp=np.zeros((len(invDate),len(allTickers)))
+        fundVal=np.zeros((len(invDate),len(allTickers)))
+        fundVal=np.zeros((len(invDate),len(allTickers)))
+        fundPMV=np.zeros((len(invDate),len(allTickers)))
+        Comp=np.zeros((len(invDate),len(allTickers)))
+        iOrderDate=0
+        for iAllDate in range(len(invDate)):
+                for iOrderB in range(iOrderDate,len(orders)):
+                    if invDate[iAllDate]>orders['date'][newIndex[iOrderB]]:
+                        tick=orders['ticker'][newIndex[iOrderB]]
+                        iStock=sum((allTickers==tick)*range(len(allTickers)))
+                        if orders['ttype'][newIndex[iOrderB]]=='B':
+                            sens=1
+                        else:
+                            sens=-1
+                            
+                        #Introduce a compensatory variable
+                        for iAllB in range(iAllDate):
+                            Comp[iAllB,iStock]=fundVal[iAllB,iStock]+sens*orders['quantity'][newIndex[iOrderB]]*orders['pru'][newIndex[iOrderB]]
+                        
+                        for iAllB in range(iAllDate,len(invDate)):
+                            
+                            fundComp[iAllB,iStock]=fundComp[iAllB,iStock]+sens*orders['quantity'][newIndex[iOrderB]]
+                            
+                            lastPrice=0#Necessary as trade day may differ from investments 
+                            if sum((priceHist['date']==invDate[iAllB]) & (priceHist['ticker']==tick))==1:
+                                dayPrice=sum(priceHist[(priceHist['date']==invDate[iAllB]) & (priceHist['ticker']==tick)]['price'])
+                                lastPrice=dayPrice
+                            else:
+                                dayPrice=lastPrice
+                                
+                            if dayPrice>0:
+                                fundVal[iAllB,iStock]=fundVal[iAllB,iStock]+sens*orders['quantity'][newIndex[iOrderB]]*dayPrice
+                                fundPMV[iAllB,iStock]=fundPMV[iAllB,iStock]+sens*orders['quantity'][newIndex[iOrderB]]*dayPrice-sens*orders['quantity'][newIndex[iOrderB]]*orders['pru'][newIndex[iOrderB]]
+                        iOrderDate+=1
+        
+        """prepare data to export"""
+        #Sum the value for the whole portfolio
+        sumFundVal=np.sum(fundVal,1)+np.sum(Comp,1)
+        sumFundPmv=np.sum(fundPMV,1)+np.sum(Comp,1)
+        
+        #Basic info at date
+        FundValLast=sumFundVal[len(sumFundVal)-1]
+        FundPmvLast=sumFundPmv[len(sumFundPmv)-1]
+        #define the reporting date vector
+        startDate=min(orders['date'])
+        reportIndex=invDate>=startDate
+        #reportDate=invDate[reportIndex]
+        reportFundValue=sumFundVal[reportIndex]
+        
+        #number of element in the reporting depend on the number of historic    
+        weekM=datetime.datetime.now()-datetime.timedelta(days=7)
+        weekM=weekM.strftime("%Y-%m-%d")
+        
+        monthM=datetime.datetime.now()-datetime.timedelta(days=30)
+        monthM=monthM.strftime("%Y-%m-%d")
+        
+        compReport=np.zeros((len(allTickers),4))
+        compReport[:,0]=fundComp[len(invDate)-1]
+        compReport[:,1]=Comp[0]/compReport[:,0]
+        compReport[:,2]=fundVal[len(invDate)-1]/compReport[:,0]
+        compReport[:,3]=fundPMV[len(invDate)-1]
+        
+        d={'tickers': allTickers, 'Number of stocks':compReport[:,0], 'PRU':compReport[:,1], 'Value':compReport[:,2], 'UGL':compReport[:,3]}
+        tabReport=pd.DataFrame(data=d)
+        #if weekM<startDate:#Less than a week of historical data
+        
+        """prepare graphs"""
+        import matplotlib.pyplot as plt
+        fig, ax = plt.subplots(nrows=1, ncols=1)
+        ax.set_facecolor('#414141')
+        fig.patch.set_facecolor('#414141')
+        plt.plot(reportFundValue)
+        
+        fig.savefig('reports/img1.jpg', facecolor='#414141', edgecolor='#414141')
+        
+    
+        """write in the template"""
+        from jinja2 import Environment, FileSystemLoader
+        env = Environment(loader=FileSystemLoader('.'))
+        template = env.get_template("reports/template.html")
+        template_vars = {"date" : today,
+                     "fundVal": FundValLast,
+                     "fundPmv": FundPmvLast,
+                     "tab": tabReport.to_html()}
+        html_out = template.render(template_vars)
+        
+        """send the mail"""
+        import smtplib
+        from email.mime.multipart import MIMEMultipart
+        from email.mime.text import MIMEText
+        from email.mime.image import MIMEImage
+        
+        me="thomas.laure.work@outlook.fr" #Insert mail adress here
+        fromaddr = me
+        toaddr ='thomas.laure@me.com'
+        msg = MIMEMultipart('related')
+        msg['From'] = me
+        msg['To'] = toaddr
+        msg['Subject'] = "Daily portfolio reporting "+today
+        msg.preamble = 'Daily Updated'
+        
+        #msgAlternative = MIMEMultipart('alternative')
+        #msg.attach(msgAlternative)
+        
+        #msgText = MIMEText('This is the alternative plain text message.')
+        #msgAlternative.attach(msgText)
+    
+        msgText=MIMEText(html_out, 'html')
+        msg.attach(msgText)
+        
+        #Add graph
+        fp = open('reports/img1.jpg', 'rb')
+        msgImage = MIMEImage(fp.read())
+        fp.close()
+        
+        # Define the image's ID as referenced above
+        msgImage.add_header('Content-ID', '<img1.jpg>')
+        msg.attach(msgImage)
+         
+        server = smtplib.SMTP('Smtp.live.com', 587) #Depend on your mail server
+        server.starttls()
+        server.login(me, "MPSI2010tlau") #Add your email account password
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddr, text)
+        server.quit()
 
+def menu():
 
-iOrderDate=0
-for iAllDate in range(len(invDate)):
-        for iOrderB in range(iOrderDate,len(orders)):
-            if invDate[iAllDate]>orders['date'][newIndex[iOrderB]]:
-                for iAllB in range(iAllDate,len(invDate)):
-                    tick=orders['ticker'][newIndex[iOrderB]]
-                    iStock=sum((allTickers==tick)*range(len(allTickers)))
-                    if orders['ttype'][newIndex[iOrderB]]=='B':
-                        sens=1
-                    else:
-                        sens=-1
-                    fundComp[iAllB,iStock]=fundComp[iAllB,iStock]+sens*orders['quantity'][newIndex[iOrderB]]
-                    
-                    lastPrice=0#Necessary as trade day may differ from investments 
-                    dayPrice=priceHist[(priceHist['date']==invDate[iAllB]) & (priceHist['ticker']==tick)]
-                    if dayPrice==[]:
-                        dayPrice=lastPrice
-                    else:
-                        lastPrice=dayPrice
-                    fundVal[iAllB,iStock]=fundVal[iAllB,iStock]+sens*orders['quantity'][newIndex[iOrderB]]*dayPrice
-                    fundPMV[iAllB,iStock]=fundPMV[iAllB,iStock]+sens*orders['quantity'][newIndex[iOrderB]]*dayPrice-sens*orders['quantity'][newIndex[iOrderB]]*orders['pru'][newIndex[iOrderB]]
-                iOrderDate+=1
+    print('Menu :')
+    print('1. restart all')
+    print('2. add order')
+    print('3. load past prices')
+    print('4. run')
+    choice=input('selection : ')
+    if choice=='1':
+        restart_all()
+    elif choice=='2':
+        add_order()
+    elif choice=='3':
+        import_past_prices()
+    elif choice=='4':
+        loop_job()
+    elif choice=='exit':
+        print('process ended')
+    else:
+        menu()
+
+if __name__ == '__main__':
+    menu()
